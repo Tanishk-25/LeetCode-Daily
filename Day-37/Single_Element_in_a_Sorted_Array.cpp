0a7867a127874ff12c2ae4396041819741f9665c
Tanishk Agarwal <150054810+Tanishk-25@users.noreply.github.com>
2026-03-14 20:26:51 +0530

    Day-37 Create Single_Element_in_a_Sorted_Array

---
+++ Day-37/Single_Element_in_a_Sorted_Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& arr) {
+        int n = arr.size();
+        if(n==1) return arr[0];
+        if(arr[0] != arr[1]) return arr[0];
+        if(arr[n-1] != arr[n-2]) return arr[n-1];
+        int l = 1, h = n-2;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]){
+                return arr[mid];
+            }
+            else if((mid % 2 == 1 && arr[mid] == arr[mid-1])
+             || (mid % 2 == 0) && arr[mid] == arr[mid+1]){
+                l = mid+1;
+            }
+            else{
+                h = mid-1;
+            }
+        }
+        return -1;
+    } 
+};
