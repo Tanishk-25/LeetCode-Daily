2ecef026657f4a5849353e6d70f1ada4418b9e0e
Tanishk Agarwal <150054810+Tanishk-25@users.noreply.github.com>
2026-05-04 22:38:10 +0530

    Day-85 Create Check_Whether_Two_Strings_are_Almost_Equivalent

---
+++ Day-85/Check_Whether_Two_Strings_are_Almost_Equivalent.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkAlmostEquivalent(string word1, string word2) {
+        int n = word1.size();
+        int freq[26] = {0};
+        for(int i = 0; i<n; i++){
+            freq[word1[i] - 'a']++;
+            freq[word2[i] - 'a']--;
+        }
+        for(int i = 0; i<26; i++){
+            if(abs(freq[i]) > 3){
+                return false;
+            }
+        }
+        return true;
+    }
+};
