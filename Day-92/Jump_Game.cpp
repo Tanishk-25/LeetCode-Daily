8d90e3262bf4247bd876c332b56c0363a3e23050
Tanishk Agarwal <150054810+Tanishk-25@users.noreply.github.com>
2026-05-11 19:45:03 +0530

    Day-92 Create Jump_Game

---
+++ Day-92/Jump_Game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int last_pos = n-1;
+        for(int i = n-2; i>=0; i--){
+            if(i + nums[i] >= last_pos){
+                last_pos = i;
+            }
+        }
+        return last_pos == 0;
+    }
+};
