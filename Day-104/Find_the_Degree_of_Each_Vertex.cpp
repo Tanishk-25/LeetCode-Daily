f0bd4a68ab233166997ad9dc4775e38070998f85
Tanishk Agarwal <150054810+Tanishk-25@users.noreply.github.com>
2026-05-23 23:43:59 +0530

    Day-104 Create Find_the_Degree_of_Each_Vertex

---
+++ Day-104/Find_the_Degree_of_Each_Vertex.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDegrees(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<int> ans(n);
+        for(int i = 0; i<n; i++){
+            int cnt = 0;
+            for(int j = 0; j<n; j++){
+                cnt += matrix[i][j];
+            }
+            ans[i] = cnt;
+        }
+        return ans;
+    }
+};
