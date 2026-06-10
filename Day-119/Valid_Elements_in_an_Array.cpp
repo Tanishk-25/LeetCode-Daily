255a68a040e76a697d2aaf4e51775a753a1c53b2
Tanishk Agarwal <150054810+Tanishk-25@users.noreply.github.com>
2026-06-10 23:58:54 +0530

    Day-119 Create Valid_Elements_in_an_Array

---
+++ Day-119/Valid_Elements_in_an_Array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findValidElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res;
+        res.push_back(nums[0]);
+
+        if(n == 1) return res;
+        vector<int> left(n), right(n);
+        left[0] = nums[0];
+        for(int i = 1; i<n; i++){
+            left[i] = max(left[i-1], nums[i]);
+        }
+        right[n-1] = nums[n-1];
+        for(int i = n-2; i>=0; i--){
+            right[i] = max(right[i+1], nums[i]);
+        }
+        for(int i = 1; i<n-1; i++){
+            if(nums[i] > left[i-1] || nums[i] > right[i+1]){
+                res.push_back(nums[i]);
+            }
+        }
+        res.push_back(nums[n-1]);
+        return res;
+    }
+};
