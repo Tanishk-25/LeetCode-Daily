60e7300ae10d82f98808162baddb3e10f6e68029
Tanishk Agarwal <150054810+Tanishk-25@users.noreply.github.com>
2026-07-03 23:46:05 +0530

    Day-134 Create Permutations

---
+++ Day-134/Permutations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void permutation(int ind, vector<int> &nums, vector<vector<int>> &res){
+        int n = nums.size();
+        if(ind == n){
+            res.push_back(nums);
+            return;
+        }
+        for(int i = ind; i<n; i++){
+            swap(nums[ind], nums[i]);
+            permutation(ind+1,nums,res);
+            swap(nums[ind], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        permutation(0,nums,res);
+        return res;
+    }
+};
