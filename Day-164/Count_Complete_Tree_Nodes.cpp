c6a79751e32bca6c3f404936a9a004ab2655d2a3
Tanishk Agarwal <150054810+Tanishk-25@users.noreply.github.com>
2026-08-15 23:59:09 +0530

    Day-164 Create Count_Complete_Tree_Nodes

---
+++ Day-164/Count_Complete_Tree_Nodes.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == NULL){
+            return NULL;
+        }
+
+        int leftcnt = countNodes(root->left);
+        int rightcnt = countNodes(root->right);
+
+        return leftcnt + rightcnt + 1;
+    }
+};
